6ffca4fd6e7522acda519273383961eaab62ec93
Javed Hussain <javed15895@gmail.com>
2025-08-05 04:36:47 +0000

    Fixed vehicle delete issue

--- app/server/src/config/database.ts
+++ app/server/src/config/database.ts
@@ -3,7 +3,7 @@ import { Sequelize } from "sequelize";
 const sequelize = new Sequelize({
   dialect: "sqlite",
   storage: `${process.env.DB_PATH || './vehicles.db'}`, // Use environment variable for database path
-  logging: false, // Set to true to see SQL queries in console
+  logging: process.env.SHOW_SQL==='true' || true,
 });
 
 export default sequelize;
