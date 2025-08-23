f8d82d57525a0c58dc85be03281567f6c67e0072
Javed Hussain <javedh.dev@gmail.com>
2025-08-23 07:34:22 +0530

    Fixed path routes issue with relative path

--- app/backend/src/db/index.ts
+++ app/backend/src/db/index.ts
@@ -8,7 +8,7 @@ const __dirname = path.dirname(__filename);
 
 const umzug = new Umzug({
   migrations: {
-    glob: path.join(__dirname, "migrations/*.ts"),
+    glob: path.join(__dirname, "migrations/*.js"),
     resolve: ({ name, path: migrationPath }) => {
       return {
         name,
